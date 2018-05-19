58517920a955bdf87f27c1933f3d160554329d58
Fred K <frederico-klein@plymouth.ac.uk>
2018-05-19 19:17:15 +0100

    trying to plan

--- scripts/make_yaml.sh
+++ scripts/make_yaml.sh
@@ -12,12 +12,17 @@ echo "** Creating combine YAML files from gummi_ee and gummi_base."
 # sleep 1m
 
 #export SRCPATH=`echo -e "${ROS_PACKAGE_PATH//:/$'\n'}" | grep /src`
-export SRCPATH=${PWD}/..
-export GUMMIBASEPATH=${SRCPATH}/gummi_leftbase_${ROS_GUMMI_LEFTBASE}
-export GUMMIEEPATH=${SRCPATH}/gummi_leftee_${ROS_GUMMI_LEFTEE}
+export GUMMI_RIGHTBASEPATH=${PWD}/../gummi_rightbase_${ROS_GUMMI_RIGHTBASE}
+export GUMMI_RIGHTEEPATH=${PWD}/../gummi_rightee_${ROS_GUMMI_RIGHTEE}
+export GUMMI_LEFTBASEPATH=${PWD}/../gummi_leftbase_${ROS_GUMMI_LEFTBASE}
+export GUMMI_LEFTEEPATH=${PWD}/../gummi_leftee_${ROS_GUMMI_LEFTEE}
+
+export GUMMI_RIG=${PWD}/../gummi_rig
 
 # LIST OF GENERATED YAML FILES AS:
 #
 # ./scripts/yaml_gen_.py <base.yaml> <ee.yaml> <combined.yaml>
 
-./scripts/yaml_gen_.py ${GUMMIBASEPATH}/config/gummi_base.yaml ${GUMMIEEPATH}/config/gummi_ee.yaml ${GUMMIBASEPATH}/config/gummi.yaml
+./scripts/yaml_gen_.py ${GUMMI_RIGHTBASEPATH}/config/joint_limits.yaml ${GUMMI_LEFTBASEPATH}/config/joint_limits.yaml ${GUMMI_RIG}/config/joint_limits.yaml
+./scripts/yaml_gen_.py ${GUMMI_RIGHTBASEPATH}/config/kinematics.yaml ${GUMMI_LEFTBASEPATH}/config/kinematics.yaml ${GUMMI_RIG}/config/kinematics.yaml
+./scripts/yaml_gen_.py ${GUMMI_RIGHTBASEPATH}/config/ompl_planning.yaml ${GUMMI_LEFTBASEPATH}/config/ompl_planning.yaml ${GUMMI_RIG}/config/ompl_planning.yaml
